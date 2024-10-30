4c89a57f03c765582b838d08f9a8b4cd5b932e1d
alicelarsson95 <alicelarsson95@hotmail.com>
2024-10-30 10:09:32 +0100

    parts2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const maxWrongGuesses = 6;
 const hangmanParts = ["ground", "scaffold", "head", "body", "arms", "legs"];
-const hangmanWords = ["apple", "grape", "peach", "berry", "lemon"];
+const hangmanWords = ["äpple", "lampa", "kaffe", "fågel", "Mössa"];
 
 const wordDisplay = document.getElementById("word-display");
 const letterInput = document.getElementById("letter-input");
